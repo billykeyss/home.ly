006877e0c5e8d0f913206f7885712cef91665fb0
Bill Huang <billhuang@live.ca>
2018-02-28 10:04:54 -0500

    remove node modules

--- config/express.js
+++ config/express.js
@@ -13,7 +13,6 @@ module.exports = (app, config) => {
   const env = process.env.NODE_ENV || 'development';
   app.locals.ENV = env;
   app.locals.ENV_DEVELOPMENT = env == 'development';
-  
   app.engine('swig', swig.renderFile);
   if (env == 'development') {
     app.set('view cache', false);
