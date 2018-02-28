39fffbfccc77e62fe88d04c5eed50de62b1e2672
Bill Huang <billhuang@live.ca>
2018-02-28 09:06:42 -0500

    remove db

--- app/models/index.js
+++ app/models/index.js
@@ -6,34 +6,34 @@ const db = {};
 
 const mysql = require('mysql2');
 
-const conn = new mysql.createConnection(config.connConfig);
+// const conn = new mysql.createConnection(config.connConfig);
 
-conn.connect(
-  function(err) {
-    if (err) {
-      console.log("!!! Cannot connect !!! Error:");
-      throw err;
-    } else {
-      console.log("Connection established.");
-    }
-  });
+// conn.connect(
+//   function(err) {
+//     if (err) {
+//       console.log("!!! Cannot connect !!! Error:");
+//       throw err;
+//     } else {
+//       console.log("Connection established.");
+//     }
+//   });
 
-console.log('test');
-fs.readdirSync(__dirname)
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== 'index.js'))
-  .forEach((file) => {
-    console.log(file);
-    // const model = sequelize['import'](path.join(__dirname, file));
-    // db[model.name] = model;
-  });
+// console.log('test');
+// fs.readdirSync(__dirname)
+//   .filter((file) => (file.indexOf('.') !== 0) && (file !== 'index.js'))
+//   .forEach((file) => {
+//     console.log(file);
+//     // const model = sequelize['import'](path.join(__dirname, file));
+//     // db[model.name] = model;
+//   });
+//
+// Object.keys(db).forEach((modelName) => {
+//   if ('associate' in db[modelName]) {
+//     db[modelName].associate(db);
+//   }
+// });
 
-Object.keys(db).forEach((modelName) => {
-  if ('associate' in db[modelName]) {
-    db[modelName].associate(db);
-  }
-});
-
-// db.sequelize = sequelize;
+// db.conn = conn;
 // db.Sequelize = Sequelize;
 
 module.exports = db;
