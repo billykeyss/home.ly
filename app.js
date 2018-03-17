d0a14a2168d38f56d0c283ff975dc630f1bd1221
Bill Huang <billhuang@live.ca>
2018-03-17 10:50:13 -0400

    add azure home hub

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const config = require('./config/config');
 const Data = require('./app/models/data');
-// var EventHubClient = require('azure-event-hubs').Client;
+var EventHubClient = require('azure-event-hubs').Client;
 var connectionString = process.env.CONN_STRING;
 
 const app = express();
@@ -23,18 +23,18 @@ var printError = function (err) {
   console.log(err.message);
 };
 
-// var client = EventHubClient.fromConnectionString(connectionString);
-// client.open()
-//     .then(client.getPartitionIds.bind(client))
-//     .then(function (partitionIds) {
-//         return partitionIds.map(function (partitionId) {
-//             return client.createReceiver('$Default', partitionId, { 'startAfterTime' : Date.now()}).then(function(receiver) {
-//                 console.log('Created partition receiver: ' + partitionId)
-//                 receiver.on('errorReceived', printError);
-//                 if(!process.env.disabled) {
-//                   receiver.on('message', Data.addDataDirectDB);
-//                 }
-//             });
-//         });
-//     })
-//     .catch(printError);
+var client = EventHubClient.fromConnectionString(connectionString);
+client.open()
+    .then(client.getPartitionIds.bind(client))
+    .then(function (partitionIds) {
+        return partitionIds.map(function (partitionId) {
+            return client.createReceiver('$Default', partitionId, { 'startAfterTime' : Date.now()}).then(function(receiver) {
+                console.log('Created partition receiver: ' + partitionId)
+                receiver.on('errorReceived', printError);
+                if(!process.env.disabled) {
+                  receiver.on('message', Data.addDataDirectDB);
+                }
+            });
+        });
+    })
+    .catch(printError);
