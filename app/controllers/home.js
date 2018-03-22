bca762628baf9cd359f13327d7c6e61d91dc94f8
Bill Huang <billhuang@live.ca>
2018-03-22 12:30:21 -0400

    Date Range filtering

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -4,49 +4,82 @@ const update = express.Router();
 const Data = require('../models/data');
 const dataUtils = require('../utils/data_utils.js');
 const moment = require('moment');
+const _ = require('lodash');
 
 module.exports = (app) => {
-  app.use('/', router);
+	app.use('/', router);
 };
 
 router.get('/', (req, res, next) => {
-  Data.getAllData().then(function(data) {
-    var dataArray = data.Items;
-    var sortedDataArray = dataUtils.groupBy(dataArray,'pi_id');
-    let chartDataObject = {};
-    let nodeArray = [];
-    var lastItem;
+	Data.getAllData().then(function(data) {
+		var dataArray = data.Items;
+		var sortedDataArray = dataUtils.groupBy(dataArray, 'pi_id');
+		var chartDataObject = {};
+    var lastDayChartDataObject = {};
+    var lastWeekChartDataObject = {};
+    var lastMonthChartDataObject = {};
+		let nodeArray = [];
+		var lastItem;
+    var emptyDataObject = {
+      labels: [],
+      humidity: [],
+      temperature: [],
+      pressure: []
+    };
 
-    for (var i in sortedDataArray) {
-      chartDataObject[i] = {};
-      chartDataObject[i].labels = [];
-      chartDataObject[i].humidity = [];
-      chartDataObject[i].temperature = [];
-      chartDataObject[i].pressure = [];
+		for (var i in sortedDataArray) {
+			chartDataObject[i] = _.cloneDeep(emptyDataObject);
+      lastDayChartDataObject[i] = _.cloneDeep(chartDataObject[i]);
+      lastWeekChartDataObject[i] = _.cloneDeep(lastDayChartDataObject[i]);
+      lastMonthChartDataObject[i] = _.cloneDeep(lastWeekChartDataObject[i]);
 
-      sortedDataArray[i].forEach( function (arrayItem) {
-          chartDataObject[i].labels.push(moment(arrayItem.date_time * 1000).format("dddd, MMMM Do YYYY, H:mm:ss"));
-          chartDataObject[i].humidity.push(arrayItem.humidity.toFixed(2));
-          chartDataObject[i].temperature.push(arrayItem.temperature.toFixed(2));
-          chartDataObject[i].pressure.push(arrayItem.pressure.toFixed(2));
-          lastItem = arrayItem;
-      });
-    }
+			sortedDataArray[i].forEach(function(arrayItem) {
+				chartDataObject[i].labels.push(moment(arrayItem.date_time * 1000).format("dddd, MMMM Do YYYY, H:mm:ss"));
+				chartDataObject[i].humidity.push(arrayItem.humidity.toFixed(2));
+				chartDataObject[i].temperature.push(arrayItem.temperature.toFixed(2));
+				chartDataObject[i].pressure.push(arrayItem.pressure.toFixed(2));
 
-    // for (const [key, value] of Object.entries(chartDataObject)) {
-    //   nodeArray.push(key);
-    // }
+				if ((Date.now() / 1000) < ((arrayItem.date_time + 24 * 60 * 60))) {
+					lastDayChartDataObject[i].labels.push(moment(arrayItem.date_time * 1000).format("dddd, MMMM Do YYYY, H:mm:ss"));
+					lastDayChartDataObject[i].humidity.push(arrayItem.humidity.toFixed(2));
+					lastDayChartDataObject[i].temperature.push(arrayItem.temperature.toFixed(2));
+					lastDayChartDataObject[i].pressure.push(arrayItem.pressure.toFixed(2));
+				}
 
-    res.render('layout', {
-      data: JSON.stringify(chartDataObject),
-      nodeArray: JSON.stringify(nodeArray),
-      lastTemperatureReading: JSON.stringify(lastItem.temperature.toFixed(2).toString()),
-      lastPressureReading: JSON.stringify(lastItem.pressure.toFixed(2).toString()),
-      lastHumidityReading: JSON.stringify(lastItem.humidity.toFixed(2).toString())
-    });
-  });
+				if ((Date.now() / 1000) < (arrayItem.date_time + 7 * 24 * 60 * 60)) {
+					lastWeekChartDataObject[i].labels.push(moment(arrayItem.date_time * 1000).format("dddd, MMMM Do YYYY, H:mm:ss"));
+					lastWeekChartDataObject[i].humidity.push(arrayItem.humidity.toFixed(2));
+					lastWeekChartDataObject[i].temperature.push(arrayItem.temperature.toFixed(2));
+					lastWeekChartDataObject[i].pressure.push(arrayItem.pressure.toFixed(2));
+				}
+
+        if ((Date.now() / 1000) < (arrayItem.date_time + 30 * 24 * 60 * 60)) {
+          lastMonthChartDataObject[i].labels.push(moment(arrayItem.date_time * 1000).format("dddd, MMMM Do YYYY, H:mm:ss"));
+          lastMonthChartDataObject[i].humidity.push(arrayItem.humidity.toFixed(2));
+          lastMonthChartDataObject[i].temperature.push(arrayItem.temperature.toFixed(2));
+          lastMonthChartDataObject[i].pressure.push(arrayItem.pressure.toFixed(2));
+        }
+				lastItem = arrayItem;
+			});
+		}
+
+		// for (const [key, value] of Object.entries(chartDataObject)) {
+		//   nodeArray.push(key);
+		// }
+
+		res.render('layout', {
+			data: JSON.stringify(chartDataObject),
+			lastDayData: JSON.stringify(lastDayChartDataObject),
+			lastWeekData: JSON.stringify(lastWeekChartDataObject),
+			lastMonthData: JSON.stringify(lastMonthChartDataObject),
+			nodeArray: JSON.stringify(nodeArray),
+			lastTemperatureReading: JSON.stringify(lastItem.temperature.toFixed(2).toString()),
+			lastPressureReading: JSON.stringify(lastItem.pressure.toFixed(2).toString()),
+			lastHumidityReading: JSON.stringify(lastItem.humidity.toFixed(2).toString())
+		});
+	});
 });
 
 router.post('/update', (req, res) => Data.addData(req, res));
 
-router.post('/update/snore', (req,res) => Data.addSnoringData(req,res));
+router.post('/update/snore', (req, res) => Data.addSnoringData(req, res));
--- public/js/main.js
+++ public/js/main.js
@@ -1,36 +1,36 @@
 function buildChartData(data, key) {
-  var timeFormat = 'dddd, MMMM Do YYYY, H:mm:ss';
+	var timeFormat = 'dddd, MMMM Do YYYY, H:mm:ss';
 
 	// Line Chart
 	var lineChartData = {
 		labels: data[key].labels,
 		datasets: [{
 				label: "Humidity",
-        backgroundColor: [
-            'rgba(155, 89, 182, 0.1)'
-        ],
-        borderColor: [
-            'rgba(155, 89, 182, 0.9)'
-        ],
+				backgroundColor: [
+					'rgba(155, 89, 182, 0.1)'
+				],
+				borderColor: [
+					'rgba(155, 89, 182, 0.9)'
+				],
 				data: data[key].humidity
 			}, {
 				label: "Temperature",
-        backgroundColor: [
-            'rgba(41, 128, 185, 0.1)'
-        ],
-        borderColor: [
-            'rgba(41, 128, 185, 0.9)'
-        ],
+				backgroundColor: [
+					'rgba(41, 128, 185, 0.1)'
+				],
+				borderColor: [
+					'rgba(41, 128, 185, 0.9)'
+				],
 				data: data[key].temperature
 			},
 			{
 				label: "Pressure",
-        backgroundColor: [
-            'rgba(12, 80, 30, 0.1)'
-        ],
-        borderColor: [
-            'rgba(12, 100, 40, 0.9)'
-        ],
+				backgroundColor: [
+					'rgba(12, 80, 30, 0.1)'
+				],
+				borderColor: [
+					'rgba(12, 100, 40, 0.9)'
+				],
 				data: data[key].pressure
 			}
 		]
@@ -42,33 +42,86 @@ window.onload = function() {
 	var ctx1 = document.getElementById("myChart").getContext("2d");
 	ctx1.height = 1800;
 	let chartData = buildChartData(dataArray, "simNodeDevice");
+  buildChart(ctx1, chartData);
+};
+
+function buildChart(ctx, chartData) {
   var timeFormat = 'dddd, MMMM Do YYYY, H:mm:ss';
 
-	window.myChart = new Chart(ctx1, {
+  window.myChart = new Chart(ctx, {
 		type: 'line',
 		data: chartData,
 		options: {
-      scales: {
-        xAxes: [{
-          type: 'time',
-          time: {
-            format: timeFormat,
-            // round: 'day'
-            tooltipFormat: 'll HH:mm'
-          },
-          scaleLabel: {
-            display: true
-          }
-        }],
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          },
-          scaleLabel: {
-            display: true
-          }
-        }]
-      }
+			scales: {
+				xAxes: [{
+					type: 'time',
+					time: {
+						format: timeFormat,
+						// round: 'day'
+						tooltipFormat: 'll HH:mm'
+					},
+					scaleLabel: {
+						display: true
+					}
+				}],
+				yAxes: [{
+					ticks: {
+						beginAtZero: true
+					},
+					scaleLabel: {
+						display: true
+					}
+				}]
+			}
 		}
 	});
-};
+}
+
+
+function getSelectedValue(id) {
+	return $("#" + id).find("dt a span.value").html();
+}
+$("button").click(function(e) {
+	var val = getSelectedValue('countries');
+	alert(val);
+});
+
+$(".dropdown dt a").click(function(e) {
+	$(".dropdown dd ul").toggle();
+	e.preventDefault();
+});
+
+$(".dropdown dd ul li a").click(function() {
+	var text = $(this).html();
+	$(".dropdown dt a span").html(text);
+	$(".dropdown dd ul").hide();
+
+
+  var ctx1 = document.getElementById("myChart").getContext("2d");
+	ctx1.height = 1800;
+	var timeFormat = 'dddd, MMMM Do YYYY, H:mm:ss';
+
+
+	if (text.includes("Day")) {
+    console.log('day chart');
+    let chartData = buildChartData(lastDayDataArray, "simNodeDevice");
+    buildChart(ctx1, chartData);
+	} else if (text.includes("Week")) {
+    console.log("week chart");
+    let chartData = buildChartData(lastWeekDataArray, "simNodeDevice");
+    buildChart(ctx1, chartData);
+	} else if (text.includes("Month")) {
+    let chartData = buildChartData(lastMonthDataArray, "simNodeDevice");
+    buildChart(ctx1, chartData);
+  } else if (text.includes('Time')) {
+    let chartData = buildChartData(dataArray, "simNodeDevice");
+    buildChart(ctx1, chartData);
+	}
+
+});
+
+$(document).bind('click', function(e) {
+	var $clicked = $(e.target);
+	if (!$clicked.parents().hasClass("dropdown"))
+		$(".dropdown dd ul").hide();
+});
